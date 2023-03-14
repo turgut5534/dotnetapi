d2e21db6dbc7b6453fad018243c98f9bc61c0193
turgut5534 <turgutsalgin5534@gmail.com>
2023-03-14 17:21:37 +0300

    first commit

---
+++ Forecast/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Forecast.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public List<Location> Locations { get; set; }
+    }
+}
---
+++ Forecast/Models/Weather.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Forecast.Models
+{
+    public class Weather
+    {
+        [Key]
+        public int ForecastId { get; set; }
+
+        [Required]
+        public decimal Temperature { get; set; }
+
+        [Required]
+        public decimal Humidity { get; set; }
+
+        [Required]
+        public decimal WindSpeed { get; set; }
+
+        [Required]
+        public string WindDirection { get; set; }
+
+        [Required]
+        public string WeatherCondition { get; set; }
+
+        [Required]
+        public DateTime ForecastDate { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        [ForeignKey("Location")]
+        public int LocationId { get; set; }
+
+        public Location Location { get; set; }
+    }
+}
